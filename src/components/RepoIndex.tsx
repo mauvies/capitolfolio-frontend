900539024345b728d084fe27d0f96d24c741f632
Mauricio Viera <mauricio.viera@trioteca.com>
2021-05-18 21:14:47 +0200

    cleans RepoIndex

--- src/components/RepoIndex.tsx
+++ src/components/RepoIndex.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
-
 import { Owner } from '../views/Browse';
 import Repo from './Repo';
 
 const RepoIndex = (props: Owner) => {
   return (
-    <React.Fragment>
-      <div className="flex flex-col">
-        {props.repos.length !== 0 &&
-          props.repos.map((repo: any) => {
-            return <Repo key={repo.id} repo={repo} />;
-          })}
-      </div>
-    </React.Fragment>
+    <div className="flex flex-col">
+      {props.repos.length !== 0 &&
+        props.repos.map((repo: any) => {
+          return <Repo key={repo.id} repo={repo} />;
+        })}
+    </div>
   );
 };
 
