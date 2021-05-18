1bbea81ecc6c6a737191e82e2ff3e455d688ad22
Mauricio Viera <mauricio.viera@trioteca.com>
2021-05-18 21:05:42 +0200

    loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+
+  return (
+    <img
+      src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/source.gif"
+      alt="Loading..."
+      className="mx-auto"
+    />
+  );
+};
+
+export default Loading;
