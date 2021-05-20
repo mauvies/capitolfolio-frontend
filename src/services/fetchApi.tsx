df6ae5bc649cb5c9aa5d4e3019c6957c0e3fecff
Mauricio Viera <mauricio.viera@trioteca.com>
2021-05-20 23:52:28 +0200

    creates favorites api requests (get and delete)

--- src/services/fetchApi.tsx
+++ src/services/fetchApi.tsx
@@ -1,38 +1,53 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { Owner, Repo } from '../views/Browse';
-
+import { FavRepoInterface } from '../views/Favorites';
 
 export const getOwnerRepos = async (
   owner: string,
   page: number = 1
 ): Promise<Owner> => {
-
   try {
-
     const response = await axios.post('http://localhost:5000/repos', {
       owner,
       page,
     });
 
     return response.data.data;
-
   } catch (error) {
     throw new Error(error);
   }
-
 };
 
-
 export const toggleRepo = async (id: Repo['id'], isFav: boolean) => {
-
   try {
     const response = isFav
       ? await axios.delete(`http://localhost:5000/favorites/${id}`)
       : await axios.post('http://localhost:5000/favorites', { id });
 
     return response.data;
-
   } catch (error) {
     throw new Error(error);
+  }
+};
+
+export const getFavRepos = async () => {
+  try {
+    const response = await axios.get<AxiosResponse<any>>(
+      'http://localhost:5000/favorites'
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const removeRepoFromFav = async (repoId: FavRepoInterface['repoId']) => {
+  try {
+    const response = await axios.delete<AxiosResponse<any>>(
+      `http://localhost:5000/favorites/${repoId}`
+    );
+    return response;
+  } catch (error) {
+    return error;
   }
 };
