bf9ea09b930f33650f53ee10a735e044f3de6971
Mauricio Viera <mauricio.viera@trioteca.com>
2021-05-20 23:53:00 +0200

    creates favorite repos view

---
+++ src/components/FavRepo.tsx
@@ -0,0 +1,56 @@
+import { Link } from 'react-router-dom';
+
+import {
+  DesktopComputerIcon,
+  EyeIcon,
+  TrashIcon,
+} from '@heroicons/react/outline';
+
+import formatDate from '../services/formatDate';
+import { FavRepoInterface } from '../views/Favorites';
+
+interface Props {
+  fav: FavRepoInterface;
+  isLoading: boolean;
+  deleteRepo: (repoId: FavRepoInterface['repoId']) => void;
+}
+
+const FavRepo: React.FC<Props> = ({ fav, isLoading, deleteRepo }) => {
+  return (
+    <div className="flex items-center py-6 pr-4 text-xs text-gray-800 border-t cursor-pointer">
+      <div className="flex flex-1">
+        <DesktopComputerIcon className="w-4 h-4 mt-2 mr-2 text-gray-600" />
+        <div className="flex-1 pr-4">
+          <Link to="/" className="text-lg text-blue-700">
+            {fav.name}
+          </Link>
+          <p className="text-base">{fav.description}</p>
+          <p className="text-xs font-semibold">
+            Language: {fav.language || 'Unknown'}
+          </p>
+          <p className="py-1 text-xs">Clone URL: {fav.cloneUrl}</p>
+          <p className="text-sm text-gray-500">
+            Updated {formatDate(fav.createdAt)}
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col px-3">
+        <Link
+          to={`/detail/${fav.repoId}`}
+          className="py-3 cursor-pointer focus:outline-none"
+        >
+          <EyeIcon className="w-6 h-6 text-gray-600" />
+        </Link>
+        <button
+          className="py-3 cursor-pointer focus:outline-none"
+          disabled={isLoading}
+          onClick={() => deleteRepo(fav.repoId)}
+        >
+          <TrashIcon className="w-6 h-6 text-gray-600" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FavRepo;
--- src/views/Favorites.tsx
+++ src/views/Favorites.tsx
@@ -1,7 +1,59 @@
+import { useEffect, useState } from 'react';
+import FavRepo from '../components/FavRepo';
+import { getFavRepos, removeRepoFromFav } from '../services/fetchApi';
+export interface FavRepoInterface {
+  repoId: string;
+  owner: string;
+  name: string;
+  description: string;
+  language: string;
+  cloneUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 const Favorites = () => {
+  const [favRepos, setFavRepos] = useState<FavRepoInterface[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [hook, setHook] = useState<boolean>(false);
+
+  useEffect(() => {
+    getFavorites();
+  }, [hook]);
+
+  const getFavorites = async () => {
+    const response = await getFavRepos();
+    setFavRepos(response.data);
+  };
+
+  const deleteRepo = async (repoId: FavRepoInterface['repoId']) => {
+    setIsLoading(true);
+    setError(false);
+    try {
+      const repoDeleted = await removeRepoFromFav(repoId);
+      setHook(!hook);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <section className="max-w-7xl mx-auto px-4 sm:px-6">
-      <p>Favorites</p>
+    <section className="max-w-5xl px-6 py-4 mx-auto lg:px-8">
+      <p className="my-6 text-lg font-semibold">Favorites repos list</p>
+      {favRepos.length !== 0 &&
+        favRepos.map((fav: FavRepoInterface) => {
+          return (
+            <FavRepo
+              key={fav.repoId}
+              fav={fav}
+              isLoading={isLoading}
+              deleteRepo={deleteRepo}
+            />
+          );
+        })}
     </section>
   );
 };
